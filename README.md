adabce431f2cb8bd40f351a42d34fe94d9a83e22
Reed Wade <reed@typist.geek.nz>
2016-05-24 23:35:37 +1200

    initial working code

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # metlink-api-maybe
+
 Some code I've got working that uses an undocumented API found inside https://www.metlink.org.nz/
+
+There seems to be one call of interest which, given a train or bus stop code returns a reasonably understandable whack of JSON.
+
+`https://www.metlink.org.nz/api/v1/StopDepartures/JOHN`
+
+Where, `JOHN` in this case is the J'vill train stop.
+
+It's not very tidy but it does seem to work. The output is ugly still but it's a start.
+
+If you happen to have a Go compiler installed then typing:
+
+```
+$ go build -o stopstat src/stopstat/main.go 
+
+$ ./stopstat -stop 7093
+Taiaroa Street (near 10)
+Notices:
+    Some cancelled services & CLOSED CBD bus stops for Massey Uni Pde, 1-1.30pm approx. Thurs 26 May. metlink.org.nz
+Services:
+    2016-05-25T06:43:00+12:00  Strathmore Park - Outbound - Wgtn Station
+    2016-05-25T06:59:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T07:13:00+12:00  Strathmore Park - Outbound - Molesworth Street
+    2016-05-25T07:19:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T07:28:00+12:00  Strathmore Park - Outbound - Wgtn Station
+    2016-05-25T07:43:00+12:00  Strathmore Park - Outbound - Molesworth Street
+    2016-05-25T07:53:00+12:00  Strathmore Park - Outbound - Wgtn Station
+    2016-05-25T07:55:00+12:00  Strathmore Park - Inbound - School Bus
+    2016-05-25T07:59:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T08:08:00+12:00  Strathmore Park - Outbound - Victoria University
+    2016-05-25T08:30:00+12:00  Strathmore Park - Outbound - Victoria University
+    2016-05-25T08:59:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T09:29:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T09:59:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T10:29:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T10:59:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T11:29:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T11:59:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T12:29:00+12:00  Strathmore Park - Outbound - Khandallah
+    2016-05-25T12:59:00+12:00  Strathmore Park - Outbound - Khandallah
+```
+
+
+The output could be a lot better.
+
---
+++ src/stopstat/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var StopCode string
+
+func init() {
+	flag.StringVar(&StopCode, "stop", "CROF", "train or bus stop code, examples: WELL, JOHN, CROF")
+}
+
+const (
+	MetLinkAPIv1StopDeparturesUrl = "https://www.metlink.org.nz/api/v1/StopDepartures/%s"
+)
+
+type MetLinkAPIv1StopDeparturesResponse struct {
+	LastModified string            `json:"LastModified"`
+	Notices      NoticesStructList `json:"Notices"`
+	Stop         StopStruct
+	Services     ServicesStructList `json:"Services"`
+}
+
+// {'OriginStopID': 'JOHN', 'Direction': 'Inbound', 'OperatorRef': 'RAIL', 'VehicleRef': None, 'DestinationStopID': 'WELL', 'Service': {'Code': 'JVL', 'Link': 'timetables\\/train\\/JVL', 'Name': 'Johnsonville Line (Johnsonville - Wellington)', 'TrimmedCode': 'JVL', 'Mode': 'Train'}, 'DisplayDeparture': '2016-05-24T19:00:00+12:00', 'IsRealtime': False, 'DepartureStatus': None, 'ServiceID': 'JVL', 'VehicleFeature': None, 'OriginStopName': 'Johnsonville Stn', 'DestinationStopName': 'WELL - All stops', 'AimedArrival': None, 'AimedDeparture': '2016-05-24T19:00:00+12:00', 'DisplayDepartureSeconds': 705, 'ExpectedDeparture': None}
+type ServicesStruct struct {
+	OriginStopID        string `json:"OriginStopID"`
+	OriginStopName      string `json:"OriginStopName"`
+	Direction           string `json:"Direction"`
+	DestinationStopID   string `json:"DestinationStopID"`
+	DestinationStopName string `json:"DestinationStopName"`
+	DisplayDeparture    string `json:"DisplayDeparture"`
+	IsRealtime          bool   `json:"IsRealtime"`
+}
+
+type ServicesStructList []*ServicesStruct
+
+func (n *ServicesStruct) String() string {
+	var realtime string
+	if n.IsRealtime {
+		realtime = "(realTime)"
+	}
+	return fmt.Sprintf("    %s %s %s - %s - %s\n", n.DisplayDeparture, realtime, n.OriginStopName, n.Direction, n.DestinationStopName)
+}
+
+func (n ServicesStructList) String() string {
+	if len(n) == 0 {
+		return ""
+	}
+	out := "Services:\n"
+	for _, v := range n {
+		out += v.String()
+	}
+	return out
+}
+
+// {"RecordedAtTime":"2016-05-24T18:48:09+12:00","MonitoringRef":"JOHN","LineRef":"","DirectionRef":"","LineNote":"Services on the JVL are experiencing delays of up to 10 mins"}
+type NoticesStruct struct {
+	RecordedAtTime string `json:"RecordedAtTime"`
+	LineNote       string `json:"LineNote"`
+}
+
+type NoticesStructList []*NoticesStruct
+
+// Stop:  {'Name': 'Johnsonville Station', 'LastModified': '2015-09-03T11:14:30+12:00', 'Sms': 'JOHN', 'Long': '174.8047433', 'Farezone': '3', 'Lat': '-41.223345', 'Icon': '\\/assets\\/StopImages\\/JOHN.jpg'}
+type StopStruct struct {
+	Name string `json:"Name"`
+}
+
+func (m *MetLinkAPIv1StopDeparturesResponse) String() string {
+	return fmt.Sprintf("%s\n%s%s\n", m.Stop.Name, m.Notices, m.Services)
+}
+
+func (n *NoticesStruct) String() string {
+	return fmt.Sprintf("    %s\n", n.LineNote)
+}
+
+func (n NoticesStructList) String() string {
+	if len(n) == 0 {
+		return ""
+	}
+	notices := "Notices:\n"
+	for _, v := range n {
+		notices += v.String()
+	}
+	return notices
+}
+
+func main() {
+
+	flag.Parse()
+
+	StopCode = strings.ToUpper(StopCode)
+
+	url := fmt.Sprintf(MetLinkAPIv1StopDeparturesUrl, StopCode)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var response MetLinkAPIv1StopDeparturesResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Print(&response)
+
+}
